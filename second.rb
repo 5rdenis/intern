86507907b1f36ddd4ab806dc79fb7ab0484a5319
5rdenis <5rdenis4ever@gmail.com>
2019-08-29 19:43:54 +0300

    second

---
+++ second.rb
@@ -0,0 +1,29 @@
+goods_hash = Hash.new{|hsh,key| hsh[key] = [] }	
+
+input = ""
+name = ""
+price = 0
+quantity = 0
+totalsum = 0
+
+print "Enter name,price,quantity(e.g.: milk,5,10);or type STOP to quit:"
+input = gets.chomp
+while input != "stop" do
+  (name, price, quantity) = input.split(",")
+  price = price.to_i
+  quantity = quantity.to_f
+
+  goods_hash[name].push price
+  goods_hash[name].push quantity
+  totalsum += price * quantity
+  print "Enter name,price,quantity(e.g.: milk,5,10);or type STOP to quit:"
+  input = gets.chomp
+end
+
+puts "Here is the content that was in the hash: "
+goods_hash.each do |key, (price, quantity)|
+
+  puts "#{key} => #{price}, #{quantity}; sum= #{price*quantity}"
+end
+
+puts "totalsum= #{totalsum}"
